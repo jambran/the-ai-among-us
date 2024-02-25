b0539a6291e17ad01c602ebb6c48ce7e71dd8b48
Jamie Brandon <jamie.brandon@dictionary.com>
2024-02-25 14:49:46 -0500

    Add test for exception logging

---
+++ tests/test_ai_among_us/utils/test_exception_logging.py
@@ -0,0 +1,22 @@
+from src.utils.exception_logging import log_exception
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging.StreamHandler()],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+def test_exception_logging(caplog):
+    @log_exception
+    def function_that_will_error():
+        return 1 / 0
+
+    try:
+        function_that_will_error()
+    except ZeroDivisionError as e:
+        pass
+
+    assert "ZeroDivisionError" in caplog.text
