a90e644070730b8fe334f685c84259d8f913b0f5
Jamie Brandon <jamie.brandon@dictionary.com>
2024-02-25 14:33:26 -0500

    Add `game` as an attribute of network

--- src/ai_among_us/gameplay/network.py
+++ src/ai_among_us/gameplay/network.py
@@ -10,13 +10,16 @@ class Network:
         self.server = settings.GAMEPLAY_SERVER_ADDRESS
         self.port = settings.GAMEPLAY_SERVER_PORT
         self.address = (self.server, self.port)
+        self.game = self.connect()
 
     def connect(self):
         try:
             self.client.connect(self.address)
-            return self.client.recv(2048).decode()
-        except:
-            pass
+            communication = self.client.recv(2048 * 2).decode()
+            return communication
+        except socket.error as e:
+            logging.error(e)
+            raise e
 
     def send(self, data):
         try:
@@ -24,3 +27,4 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             logging.error(e)
+            raise e
